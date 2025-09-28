9dec1da9bbff22f04197f9350dc9bad7765ec4fc
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-28 15:38:20 +0200

    feat: initial commit for app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Ứng dụng Quản lý Chuyến đi Làm việc Từ xa</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
