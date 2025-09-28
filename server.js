2add1abf77e2d04dbbbced8756aada2630b74c01
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-28 15:38:18 +0200

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/ung-dung', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Kết nối cơ sở dữ liệu thành công!');
+}).catch(err => {
+  console.error('Kết nối cơ sở dữ liệu thất bại:', err);
+});
+
+app.listen(port, () => {
+  console.log(`Server đang lắng nghe tại http://localhost:${port}`);
+});
